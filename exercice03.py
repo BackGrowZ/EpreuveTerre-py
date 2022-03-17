660cb49bfbfc7cec47efed843f1e659055ffa935
Bardevoir <nicolas.huard31@gmail.com>
2022-03-17 02:57:04 +0100

    Ajout de l'exercice 4

---
+++ exercice03.py
@@ -0,0 +1,18 @@
+# Afficher l'alphabet à partir d'une lettre donnée
+# Condition : utiliser une boucle
+
+import sys
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+try:
+    arg_user = str(sys.argv[1])
+    arg_pos = alphabet.index(arg_user)
+except IndexError:
+    print("Veuillez rentrer une lettre en minuscule")
+except ValueError:
+    print("Veuillez rentrer une lettre en minuscule")
+else:    
+    for i in range(arg_pos, len(alphabet)):
+        print(alphabet[i], end="")
+        
+    print()
