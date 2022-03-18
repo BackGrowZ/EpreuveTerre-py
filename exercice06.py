9c712874b9112e24a298146f84237a69563b570e
Bardevoir <nicolas.huard31@gmail.com>
2022-03-18 03:19:48 +0100

    Ajout du 7e exercice

---
+++ exercice06.py
@@ -0,0 +1,23 @@
+# Affiche l'inverse d'une chaîne de caractères passée en argument
+
+import sys
+
+
+def inverser_texte(chaine):
+    chaine_retournee = []
+    for i in chaine:
+        chaine_retournee.insert(0,i)
+    for j in chaine_retournee:
+        print(j, end="")
+    print()
+        
+try:
+    txt_user = sys.argv[1]
+except IndexError:
+    print("Veuillez passer un argument")
+else:
+    if not len(sys.argv) == 2:
+        print("Veuillez rentrer une unique chaîne de caractère")
+    else:
+       inverser_texte(txt_user)
+        
