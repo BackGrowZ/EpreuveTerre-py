c6c673a2fa0711088d857552cc28c9728652f3e8
Bardevoir <nicolas.huard31@gmail.com>
2022-03-22 00:04:58 +0100

    Optimisation code

--- exercice07.py
+++ exercice07.py
@@ -4,18 +4,22 @@ import sys
 
 def compter_chaine(chaine):
     count = 0
-    for i in chaine:
-        count = count + 1
-    return str(count)
+    if not len(sys.argv) == 2:
+        print("Veuillez rentrer une unique chaîne de caractère")
+        return
+    elif txt_user.isdigit():
+        print("Veuillez rentrer de chaîne correcte")
+        return
+    else:
+        for i in chaine:
+            count = count + 1
+        return str(count)
+    
 
 try:
     txt_user = sys.argv[1]
 except IndexError:
     print("Veuillez passer un argument")
 else:
-    if not len(sys.argv) == 2:
-        print("Veuillez rentrer une unique chaîne de caractère")
-    elif txt_user.isdigit():
-        print("Veuillez rentrer de chaîne correcte")
-    else:
+    if not compter_chaine(txt_user) == None:
         print(compter_chaine(txt_user))
