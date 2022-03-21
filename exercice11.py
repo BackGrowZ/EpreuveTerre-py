572ded13b5266768284855ae7fb29b2a2e5417fb
Bardevoir <nicolas.huard31@gmail.com>
2022-03-22 00:39:06 +0100

    Optimisation code

--- exercice11.py
+++ exercice11.py
@@ -5,25 +5,27 @@ import sys
 
 def convertir(h, m):
     resultat = ""
-    if int(h) < 0 or int(h) > 24 or len(str(h)) > 2:
-        print("Veuillez rentrer une heure correcte")
+    if h < 0 or h > 24 or len(str(h)) > 2:
+        print("Veuillez rentrer une valeur correcte pour l'heure")
         return
-    elif int(m) < 0 or int(m) > 60 or not len(str(m)) == 2:
-        print("Veuillez rentrer une minute correcte")
+    elif m < 0 or m > 60 or not len(str(m)) == 2:
+        print("Veuillez rentrer une valeur correcte pour la minute")
         return        
-    if int(h) <= 12:
+    if h <= 12:
         resultat = str(h) + ":" + str(minute) + "AM"
     else:
-        h = int(h) - 12
+        h = h - 12
         resultat = str(h) + ":" + str(minute) + "PM"
     return resultat
 
 try:
     heure_24 = sys.argv[1].split(":")
-    heure = heure_24[0]
-    minute = heure_24[1]
+    heure = int(heure_24[0])
+    minute = int(heure_24[1])
 except IndexError:
         print("Veuillez passer un argument")
+except ValueError:
+        print("Veuillez rentrer une heure correcte")
 else:
     if not convertir(heure, minute) == None:
         print(convertir(heure, minute))
